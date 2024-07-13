d56f061ff2413004e023987c6eab88b39b9d4803
PASI1121 <pasinduniranjana486@gmail.com>
2024-07-13 12:34:06 +0530

    Add files via upload
    Power BI
    MySQL

---
+++ Sales Insights Data Analysis Project.sql
@@ -0,0 +1,3 @@
+select sales.transactions.*,sales.date.* from sales.transactions inner join sales.date on sales.transactions.order_date=sales.date.date where sales.date.year=2020;
+select sum(sales.transactions.sales_amount) from sales.transactions inner join sales.date on sales.transactions.order_date=sales.date.date where sales.date.year=2020;
+select * from sales.transactions where sales_amount<=0;
